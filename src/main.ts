c2700c7ce98b93c4ad4e0179762c9dc1460a5aa1
FatPaper-1874 <360655753@qq.com>
2024-04-16 15:52:15 +0800

    优化fontawesome的加载方式

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 /* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faShieldHalved, faSplotch, faFan, faBook, faMusic, faGuitar, faMugSaucer } from "@fortawesome/free-solid-svg-icons";
 
 
-library.add(fas);
+library.add(faShieldHalved, faSplotch, faFan, faBook, faMusic, faGuitar, faMugSaucer);
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
