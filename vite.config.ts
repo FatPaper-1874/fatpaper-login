ba60df37bb50b2be1059f5393eb529a7498f40c7
FatPaper-1874 <360655753@qq.com>
2024-09-28 15:54:45 +0800

    去除生产环境的打印

--- vite.config.ts
+++ vite.config.ts
@@ -6,41 +6,46 @@ import viteCompression from "vite-plugin-compression";
 import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		vue(),
-		viteCompression(),
-		visualizer({
-			gzipSize: true,
-			brotliSize: true,
-			emitFile: false,
-			filename: "test.html", //分析图生成的文件名
-			open: true, //如果存在本地服务端口，将在打包后自动展示
-		}),
-	],
-	build: {
-		rollupOptions: {
-			plugins: [
-				externalGlobals({
-					vue: "Vue",
-					three: "THREE",
-				}),
+export default defineConfig(({ command }) => {
+	return {
+		plugins: [
+			vue(),
+			viteCompression(),
+			visualizer({
+				gzipSize: true,
+				brotliSize: true,
+				emitFile: false,
+				filename: "test.html", //分析图生成的文件名
+				open: true, //如果存在本地服务端口，将在打包后自动展示
+			}),
+		],
+		build: {
+			rollupOptions: {
+				plugins: [
+					externalGlobals({
+						vue: "Vue",
+						three: "THREE",
+					}),
+				],
+			},
+		},
+		server: {
+			port: 81,
+		},
+		resolve: {
+			alias: [
+				{
+					find: "@",
+					replacement: path.resolve(path.dirname("./"), "src"),
+				},
+				{
+					find: "@G",
+					replacement: path.resolve(path.dirname("./"), "./"),
+				},
 			],
 		},
-	},
-	server: {
-		port: 81,
-	},
-	resolve: {
-		alias: [
-			{
-				find: "@",
-				replacement: path.resolve(path.dirname("./"), "src"),
-			},
-			{
-				find: "@G",
-				replacement: path.resolve(path.dirname("./"), "./"),
-			},
-		],
-	},
+		esbuild: {
+			drop: command === "build" ? ["console", "debugger"] : [],
+		},
+	};
 });
